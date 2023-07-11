18a9d3818319a6df6dbd5b96e09784ce80b9ac6d
ctrl-alter-ego <adyharold+github@gmail.com>
2023-07-11 17:34:38 +0100

    Uses AJAX to dynamically fetch header and footer

--- js/script.js
+++ js/script.js
@@ -10,3 +10,19 @@ window.addEventListener('scroll', function() {
     }
   });
   
+
+  fetch("header.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("header").innerHTML = data;
+  });
+
+fetch("footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data;
+  });
