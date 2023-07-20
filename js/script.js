19964aea7ce1629d001c47cd37bb3c4a116e03cd
ctrl-alter-ego <adyharold+github@gmail.com>
2023-07-20 17:19:05 +0100

    Updates privacy policy

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ window.addEventListener('scroll', function() {
     }
   });
   
-
+// fetch page furniture
   fetch("header.html")
   .then(response => {
     return response.text()
@@ -26,3 +26,32 @@ fetch("footer.html")
   .then(data => {
     document.querySelector("footer").innerHTML = data;
   });
+
+  // today's date
+  document.addEventListener('DOMContentLoaded', function() {
+    const months = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    const addOrdinalSuffix = function(number) {
+        if (number % 10 === 1 && number % 100 !== 11) {
+            return number + 'st';
+        } else if (number % 10 === 2 && number % 100 !== 12) {
+            return number + 'nd';
+        } else if (number % 10 === 3 && number % 100 !== 13) {
+            return number + 'rd';
+        } else {
+            return number + 'th';
+        }
+    };
+
+    const today = new Date();
+    const day = addOrdinalSuffix(today.getDate());
+    const month = months[today.getMonth()];
+    const year = today.getFullYear();
+
+    const formattedDate = `${day} ${month} ${year}`;
+
+    document.getElementById('date').innerText = formattedDate;
+});
